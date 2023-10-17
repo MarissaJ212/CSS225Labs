a984aed0a5518f2a42ae04b225cf2f72703a3a1b
MarissaJ212 <146869168+MarissaJ212@users.noreply.github.com>
2023-10-17 12:53:31 -0500

    Add files via upload

---
+++ Mod4TeamAssignment.py
@@ -0,0 +1,39 @@
+#Marissa Jones
+#10-17-2023
+
+#Module 4 Team Assignment
+print("please choose your option from the list below:")
+
+print("1. Wake up and study")
+print("2. Take out garbage")
+print("3. Wash dishes")
+print("4. Do the laundry")
+print("5. Go to bed")
+print("0. Exit")
+
+choice= int(input("\nyour choice:"))
+
+if choice == 1.:
+    print("You have selected option 1.")
+    print("Now, let's study!")
+
+elif choice == 2.:
+    print("You have selected option 2.")
+    print("It's time to take out the trash!")
+
+elif choice == 3.:
+    print("You have selected option 3.")
+    print("Let's wash the dishes.")
+
+elif choice == 4.:
+    print("You have selected option 4.")
+    print("It's time to do the laundry")
+
+elif choice == 5.:
+    print("You have selected option 5.")
+    print("Good night.")
+
+elif choice == 0.:
+    exit()
+
+
